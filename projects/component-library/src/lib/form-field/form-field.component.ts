86ec76036a128d16c6d7c5f3f52cf12b3e8ba395
Soma Osvay <osvays@starschema.net>
2024-10-20 20:55:28 +0200

    Form Field Added

---
+++ projects/component-library/src/lib/form-field/form-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-form-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.scss'
+})
+export class FormFieldComponent {
+
+}
