df4230535eee61e2bc0ef7f928b45cba1577ef9d
Soma Osvay <osvaysoma@gmail.com>
2025-05-24 00:05:58 +0200

    changed panel title to be suitable for simple usecases

--- projects/component-library/src/lib/expansion-panel/expansion-panel-title.component.ts
+++ projects/component-library/src/lib/expansion-panel/expansion-panel-title.component.ts
@@ -3,6 +3,13 @@ import { ChangeDetectionStrategy, Component, Directive } from "@angular/core";
 @Component({
     selector: 'tab-expansion-panel-title',
     template: `<ng-content></ng-content>`,
+    styles: `
+        :host {
+            align-items: center;
+            gap: 0.25rem;
+            display: flex;
+        }
+    `,
     standalone: false,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
